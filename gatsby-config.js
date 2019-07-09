6be538dccc71fb5f31c77c95cc35fad093948b6d
Mike Tallerico <mtallerico1@gmail.com>
2019-07-08 21:01:17 -0400

    added styled component plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,32 @@
-let contentfulConfig
+let contentfulConfig;
 
 try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
+	// Load the Contentful config from the .contentful.json
+	contentfulConfig = require('./.contentful');
 } catch (_) {}
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
+	spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+	accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+};
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
+	throw new Error('Contentful spaceId and the delivery token need to be provided.');
 }
 
 module.exports = {
-  pathPrefix: '/gatsby-contentful-starter',
-  plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
-    }
-  ],
-}
+	pathPrefix: '/gatsby-contentful-starter',
+	plugins: [
+		'gatsby-transformer-remark',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-source-contentful',
+			options: contentfulConfig,
+		},
+		`gatsby-plugin-styled-components`,
+	],
+};
